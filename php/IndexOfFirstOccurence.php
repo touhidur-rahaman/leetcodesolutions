6c31bd807f347e0684a85efd14b23cc86f828694
touhidur-rahaman <touhidur.rahaman.antu@gmail.com>
2025-07-29 09:33:07 +0600

    index of occurence

--- php/IndexOfFirstOccurence.php
+++ php/IndexOfFirstOccurence.php
@@ -3,6 +3,7 @@
 class IndexOfFirstOccurence {
 
     /**
+     * problem url = https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/description/
      * @param String $haystack
      * @param String $needle
      * @return Integer
